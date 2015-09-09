1734d31f8a827f4a32438816ed18cd8b83a06df5
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-09 15:02:21 +0800

    Addded routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,31 @@ Route::get('/', function() {
 });
 
 Route::get('login', 'LoginController@index');
+
+Route::resource('projects'                      , 'ProjectController');
+Route::resource('projects.overview'             , 'ProjectOverviewController');
+Route::resource('projects.selfsubmissions'      , 'ProjectSelfSubmissionsController');
+Route::resource('projects.likeitlist'           , 'ProjectLikeItListController');
+Route::resource('projects.likeitlistpublic'		, 'ProjectLikeItListPublicController');
+Route::resource('projects.rolematches'          , 'ProjectRoleMatchesController');
+Route::resource('projects.worksheets'           , 'ProjectWorksheetsController');
+Route::resource('project.auditions'             , 'ProjectAuditionsController');
+
+Route::resource('quickpost'                     , 'QuickPostController');
+
+/** talents **/
+Route::resource('talents'                       , 'TalentController');
+Route::resource('favetalents'                   , 'FaveTalentController');
+
+Route::resource('projects.jobs'                 , 'JobController');
+Route::resource('settings'                      , 'SettingController');
+
+/** schedule **/
+Route::resource('schedules'                     , 'ScheduleController');
+Route::resource('projects.schedules'            , 'ProjectSchedulesController');
+Route::resource('projects.schedules.timeframes' , 'TimeframeController');
+Route::resource('projects.schedules.auditions'  , 'AuditionController');
+
+/** Conversation **/
+Route::resource('conversations'                 , 'ConversationController');
+Route::resource('conversations.messages'        , 'ConversationMessageController');
