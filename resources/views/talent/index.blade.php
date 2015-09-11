6df5f2ad549149dc455c051059af7192a9a64015
James Harold <jamesharold.utech@gmail.com>
2015-09-11 17:23:30 +0800

    talents page draft1

--- resources/views/talent/index.blade.php
+++ resources/views/talent/index.blade.php
@@ -15,8 +15,43 @@
 				</h1>
 			</div>
 		</div>
-		<div class="talents-content">
-			
+		<div class="talents-search-filter-content">
+			<div class="row-fluid">
+				<div class="col-md-3">
+					<div class="panel panel-talents-search">
+						<div class="panel-heading">
+							<span class="panel-title">Refine Search</span>
+							<div class="panel-heading-controls">
+								<div class="panel-heading-icon"><i class="fa fa-chevron-left"></i></div>
+							</div>
+						</div>
+						<div class="panel-body">
+							<div class="location">
+								<div class="panel panel-transparent no-margin-b">
+									<div class="panel-heading no-padding-hr padding-bottom-zero-small">
+										<div class="panel-title"><strong>Location</strong></div>
+									</div>
+									<div class="panel-body padding-normal no-border-hr no-padding-hr">
+										<div class="tab-content no-padding-hr padding-top-zero-zz-lg no-padding-b">
+											<div class="tab-pane fade active in">
+												<label class="text-semibold">Enter Zip Code to Auto Select Markets</label>
+												<div class="row">
+													<div class="col-md-12">
+														<div class="input-group">
+															<input type="text" class="form-control" name="zip_code" placeholder="Enter Zip Code" id="zip-code" max="5" maxlength="5">
+														</div>
+													</div>
+												</div>
+											</div>
+											
+										</div> <!-- ./tab-content -->
+									</div> <!-- ./panel-body -->
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 @stop
