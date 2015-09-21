e6473048d1d3213126d2e1d1c16a97d56776ed2f
Daniel Cabale <dc.utech@gmail.com>
2015-09-21 18:57:58 +0800

    issue-25-v1

--- resources/js/controllers/projects.js
+++ resources/js/controllers/projects.js
@@ -4,4 +4,5 @@ module.exports = function(core, user) {
 	var handler = require('../event-handlers/projects.js')(core, user);
 
 	$(document).on('click', '.view-role-matches', handler.roleMatches);
+
 }
