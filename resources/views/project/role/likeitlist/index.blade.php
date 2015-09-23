be50ce6e0b7c76208e4ee4fd9cfa153b2278cea7
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-23 14:35:51 +0800

    Fixed modals.

--- resources/views/project/role/likeitlist/index.blade.php
+++ resources/views/project/role/likeitlist/index.blade.php
@@ -16,7 +16,7 @@
 						</div>
 						<div class="col-md-6 text-align-right">
 							<a data-bind="/projects/<%= casting_id %>/roles/<%= role.role_id %>/public-like-it-list" class="btn btn-primary" data-toggle="modal">Share Like It List</a>
-							<a href="" class="btn btn-primary"><i class="fa fa-envelope-o"></i> Invite to Audition</a>
+							<button data-toggle="modal" data-target="#share-like-it-list" class="btn btn-primary"><i class="fa fa-envelope-o"></i> Invite to Audition</button>
 						</div>
 					</div>
 
