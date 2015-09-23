073032d84773449675379920bd63697c30c5e5dd
Josh Dabuet <joshd.utech@gmail.com>
2015-09-23 16:26:43 +0800

    Delete Project functionality

--- resources/js/controllers/projects.js
+++ resources/js/controllers/projects.js
@@ -1,5 +1,6 @@
 module.exports = function(core, user) {
 	var handler = require('../event-handlers/projects.js')(core, user);
 
+	$(document).on('click', '.project-item-delete', handler.deleteProject);
 	$(document).on('click', '.delete-role', handler.deleteRole);
 }
