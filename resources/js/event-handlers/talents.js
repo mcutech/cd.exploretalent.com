32fbe9cdd1b26c08fa99b33268b439562b42e3d3
Daniel Cabale <dc.utech@gmail.com>
2015-09-28 18:22:40 +0800

    issue-46-v2

--- resources/js/event-handlers/talents.js
+++ resources/js/event-handlers/talents.js
@@ -28,25 +28,36 @@ handler.prototype.refreshAdvancedSearchValues = function() {
 		});
 }
 
-handler.prototype.refreshList = function(group){
+handler.prototype.refreshList = function(){
+	var qs = self.core.service.query_string();
 	var data = {
-		//page : $.queryToObject().talent_page ? $.queryToObject().talent_page : 1,
-		withs : [
+		withs	: [
+			'user',
 			'bam_talentinfo1',
 			'bam_talentinfo2',
-			'bam_talent_resume',
-			'bam_talent_media2',
+			'bam_talent_media2'
 		],
-		wheres : group,
-		//wheres : [['join', 'talentinfo1', 'talentci.talentnum', '=', 'talentinfo1.talentnum'],groups,
-		orders : {
-			'talentci.talentnum' : 'asc',
-			'talentinfo1.rating' : 'desc'
-		},
-		per_page : 12,
-		from : 1,
-		to : 12
-	};
+		wheres : [
+			[ 'leftJoin', 'laret_users', 'laret_users.bam_talentnum', '=', 'talentci.talentnum' ],
+			[ 'select', '*', 'laret_favorite_talents.id AS favorite', 's1.id AS schedule_id1', 's2.id AS schedule_id2', 's1.rating AS rating1', 's2.rating AS rating2' ],
+			[ 'leftJoin', 'laret_favorite_talents', 'laret_favorite_talents.bam_talentnum', '=', 'talentci.talentnum' ],
+			[ 'leftJoin', 'laret_schedules AS s1', 's1.invitee_id', '=', 'laret_users.id' ],
+			[ 'leftJoin', 'laret_schedules AS s2', 's2.inviter_id', '=', 'laret_users.id' ],
+			[ 'where', [
+					[ 'where', 'laret_favorite_talents.bam_cd_user_id', '=', self.user.bam_cd_user_id ],
+					[ 'orWhere', [
+							[ 'whereNull', 'laret_favorite_talents.bam_cd_user_id']
+						]
+					]
+				]
+			],
+		],
+		page : qs.page || 0
+	}
+
+	if (self.filter) {
+		data.wheres = data.wheres.concat(self.filter);
+	}
 		return self.core.resource.talent.get(data)
 			.then(function(list){
 				console.log(list);
@@ -57,7 +68,7 @@ handler.prototype.refreshList = function(group){
 					name 	 : 'talent_page',
 					per_page : 24
 				});*/
-				self.core.service.databind('.talents-search-result', list);
+				self.core.service.databind('#talent-result', list);
 				return $.when();
 			});
 };
