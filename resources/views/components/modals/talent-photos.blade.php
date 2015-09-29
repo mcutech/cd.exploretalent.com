81011e42a60bf20a7912daba953c146b64af5d57
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-09-29 16:49:11 +0800

    Fixed modals.remove unused files

--- resources/views/components/modals/talent-photos.blade.php
+++ resources/views/components/modals/talent-photos.blade.php
@@ -1,11 +1,26 @@
-<div id="talent-photos-modal" class="modal fade" tabindex="-1" role="dialog" style="display: none;">
+<div id="talent-photos-modal" class="modal fade modal-photos talent-photo-modal-wrapper" tabindex="-1" role="dialog" style="display: none;">
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-				<h4 class="modal-title">Talent photos</h4>
+				<h4 class="modal-title">Angelique Augustos's Photo</h4>
 			</div>
-			<div class="modal-body">...</div>
+			<div class="modal-body">
+				<div class="photos-wrap text-center">
+					<div class="row">
+						@for ($i = 0; $i < 16; $i++)
+						<div class="col-md-3 margin-bottom-normal-medium image-container">
+							<div class="photo-item">
+								<div class="image-container">
+									<img src="http://images5.fanpop.com/image/photos/25800000/Hayley-hayley-williams-25813741-526-670.png" class="img-responsive">
+								</div>
+							</div>
+						</div>
+						@endfor
+
+					</div>
+			    </div>
+			</div>
 		</div> <!-- / .modal-content -->
 	</div> <!-- / .modal-dialog -->
 </div> <!-- / .modal -->
