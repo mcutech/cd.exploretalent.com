c0ab41e5d0bb471a6b7034dd9145e93bc46f24b1
Aimee <aimee.utech@gmail.com>
2015-09-30 21:24:24 +0800

    talent modal-resume

---
+++ resources/js/components/talent-resume-modal.js
@@ -0,0 +1,30 @@
+
+'use strict';
+
+module.exports = function(core, user) {
+	$(document).on('click', '#talent-resume', function(e) {
+		var id;
+
+		if ($(e.target).is('a')) {
+			id = $(e.target).attr('data-id');
+		}
+		else {
+			id = $(e.target).parents('a').attr('data-id');
+		}
+
+		var data = {
+			talentId :id,
+			query : [
+				['with', 'bam_talent_media2'],
+				['with', 'bam_talentinfo1'],
+				['with', 'bam_talentinfo2']
+			]
+		};
+
+		return self.core.resource.talent.get(data)
+			.then(function(talent) {
+			console.log(talent);
+				self.core.service.databind('#talent-resume-modal', talent);
+			});
+	});
+}
