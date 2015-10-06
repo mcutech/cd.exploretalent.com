abb6cd18bce8fde4e069f74f446ad1bf0aa5c24d
Josh Dabuet <joshd.utech@gmail.com>
2015-10-06 19:57:35 +0800

    Role matches (Pre-submissions) page add/edit note functionality

--- resources/js/controllers/role.match.js
+++ resources/js/controllers/role.match.js
@@ -7,4 +7,10 @@ module.exports = function(core, user, projectId, roleId) {
 	$('#remove-all-likeitlist').on('click', handler.removeAllLikeItList);
 	$('#roles-list').on('change', handler.changeRole);
 	$(document).on('click', '.rating-button', handler.rateSchedule);
+
+	$(document).on('click', '.add-casting-note', handler.getDetailsForAddNoteModal);
+	$('.add-note-for-talent').on('click', handler.addNoteForTalent);
+
+	$(document).on('click', '.edit-note-link', handler.getDetailsForEditNoteModal);
+	$('.edit-note-for-talent').on('click', handler.editNoteForTalent);
 }
