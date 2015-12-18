49e061832fc458d6e56e948d3098c3599a3691e0
Sigmund Burgos <sigmund.burgos@gmail.com>
2015-12-18 16:21:38 +0800

    Fixed favorite talents not loading.

--- resources/js/event-handlers/talent.favorite.js
+++ resources/js/event-handlers/talent.favorite.js
@@ -13,27 +13,8 @@ handler.prototype.refresh = function(){
 			'bam_talentci.bam_talentinfo2',
 			'bam_talentci.bam_talent_media2'
 		],
-		/*wheres : [
-			[ 'leftJoin', 'laret_users', 'laret_users.bam_talentnum', '=', 'talentci.talentnum' ],
-			[ 'select', '*', 'laret_favorite_talents.id AS favorite', 's1.id AS schedule_id1', 's2.id AS schedule_id2', 's1.rating AS rating1', 's2.rating AS rating2' ],
-			[ 'leftJoin', 'laret_favorite_talents', 'laret_favorite_talents.bam_talentnum', '=', 'talentci.talentnum' ],
-			[ 'leftJoin', 'laret_schedules AS s1', 's1.invitee_id', '=', 'laret_users.id' ],
-			[ 'leftJoin', 'laret_schedules AS s2', 's2.inviter_id', '=', 'laret_users.id' ],
-			[ 'where', [
-					[ 'where', 'laret_favorite_talents.bam_cd_user_id', '=', self.user.bam_cd_user_id ],
-					[ 'orWhere', [
-							[ 'whereNull', 'laret_favorite_talents.bam_cd_user_id']
-						]
-					]
-				]
-			],
-		],*/
 		page : qs.page || 0
 	}
-
-	/*if (self.filter) {
-		data.wheres = data.wheres.concat(self.filter);
-	}*/
 
 	self.core.resource.favorite_talent.get(data)
 	.then(function(result){
@@ -42,14 +23,11 @@ handler.prototype.refresh = function(){
 		}
 		_.each(result.data, function(res){
 			res.bam_talentci.favorite = res.id;
-			res.bam_talentci.schedule_id1 = null;
-			res.bam_talentci.schedule_id2 = null;
+			res.bam_talentci.schedule ={};
 			res.bam_talentci.rating = null;
-			res.bam_talentci.rating1 = null;
-			res.bam_talentci.rating2 = null;
-			res.bam_talentci.schedule = null;
 			talent.data.push(res.bam_talentci);
 		});
+		console.log(talent);
 		self.core.service.databind('#favorite-result', talent);
 		self.talent = talent;
 		$('#loading-div').hide();
