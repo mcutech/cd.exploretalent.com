0b4b0704dd9f06b14e97b9067a9b2254d8e2ea89
Daniel Cabale <dc.utech@gmail.com>
2016-01-16 01:35:08 +0800

    issue-130-v1

---
+++ resources/views/layouts/components/stationary-alert.blade.php
@@ -0,0 +1,5 @@
+<div id="top-alert-div" class="alert alert-warning border-radius-zero
+margin-zero text-center padding-xs" style="z-index: 1; max-height: 40px; min-height:40px; overflow: hidden; display: none;" data-bind-target="visibility" data-bind="<%= (!body) ? '0' : '1' %>">
+	<span data-bind="<%= body %>" data-bind-target="html"></span>
+</div>
+
