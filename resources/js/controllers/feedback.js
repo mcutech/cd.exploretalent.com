d764a133beaba4ef84b4c56d5df2148347844991
James Harold Canque <jamesharold.utech@gmail.com>
2016-02-09 17:13:19 +0800

    Add Feedback Upload File Buttong Change UI

--- resources/js/controllers/feedback.js
+++ resources/js/controllers/feedback.js
@@ -3,6 +3,10 @@ module.exports = function(core, user) {
 
 	$(document).on('click', '#add-feedback-btn', handler.addNewFeedback);
 
+	// upload file input design
+	$('#upload-file-name').pixelFileInput({ placeholder: '(No File Selected) Click here to select file... '});
+	$(".pfi-choose")[0].innerHTML = "<i class=\"fa fa-paperclip\"></i> Choose File";
+
 	// reset modal contents on close
 	$("#add-feedback-modal").on("hidden.bs.modal", function(){
 	    $("#feedback-message").val("");
