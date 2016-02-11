030715e4b403f4bb33992882760f637df684d8f6
Sigmund Burgos <sigmund.burgos@gmail.com>
2016-02-11 18:39:56 +0800

    Fixed bug in self submission.

--- resources/js/event-handlers/role.selfsubmission.js
+++ resources/js/event-handlers/role.selfsubmission.js
@@ -149,7 +149,7 @@ handler.prototype.getFilters = function() {
 	var qs = self.core.service.query_string();
 	var data = {
 		q : [
-			[ 'select', 'bam.laret_schedules.id' ],
+			[ 'select', 'bam.laret_schedules.*' ],
 			[ 'join', 'bam.laret_users', 'bam.laret_users.bam_talentnum', '=', 'search.talents.talentnum' ],
 			[ 'join', 'bam.laret_schedules', 'bam.laret_schedules.invitee_id', '=', 'bam.laret_users.id' ],
 			[ 'where', 'bam.laret_schedules.submission', '=', 1 ],
