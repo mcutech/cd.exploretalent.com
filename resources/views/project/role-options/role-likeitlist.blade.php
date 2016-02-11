af9a82836342d9740bcec4ebfc4445d82bf1f889
James Harold Canque <jamesharold.utech@gmail.com>
2016-02-11 18:26:48 +0800

    find talents page Find talent Modal and Like it List Modal

--- resources/views/project/role-options/role-likeitlist.blade.php
+++ resources/views/project/role-options/role-likeitlist.blade.php
@@ -1,28 +1,21 @@
-<div class="self-submissions-wrapper self-submissions-talent-view-wrapper self-submissions-notes-view-wrapper">
+<div class="like-it-list-talents-wrapper like-it-list-page-wrapper">
 	<div class="talents-search-filter-content">
 		@parent
-		<div class="row-fluid clearfix"> @include('components.talent-filter')
+		<div class="row-fluid clearfix">
 
-			<div id="self-submissions" class="col-md-9 talents-search-result">
+			<div id="like-it-list" class="col-md-12 talents-search-result">
 				<div class="row-fluid clearfix top-results-heading margin-bottom-normal">
 					<div class="col-md-6">
-						<div id="like-it-list-div" class="panel margin-bottom-small">
-							<div class="padding-normal" data-bind="<%= role.schedule_import ? 0 : 1 %>" data-bind-target="visibility">
-								<h5 class="margin-top-zero display-inline-block margin-right-small"><span class="text-normal">Self Submissions for Role:</span> <span data-bind="<%= role.name %>"></span></h5>
-								<button id="rate-all-button" class="btn btn-defaut">Add all to Like It List</button>
-								<span class="text-primary"><a data-bind="/projects/<%= casting_id %>/roles/<%= role.role_id %>/like-it-list" href="">You have <span data-bind="<%= role.likeitlist.total %>"></span> in your like it list.</a></span>
-							</div>
-							<div class="padding-normal" data-bind="<%= role.schedule_import ? 1 : 0 %>" data-bind-target="visibility">
-								<span class="text-primary" data-bind="Adding <%= role.schedule_import ? role.schedule_import.count_done : 0 %> of <%= role.schedule_import ? role.schedule_import.count_total : 0 %> talents to Like It List. <%= role.schedule_import ? role.schedule_import.estimated_time : '' %>"></span>
+						<div class="float-left">
+							<div id="like-it-list-pagination">
 							</div>
 						</div>
 					</div>
-					<div class="col-md-6 padding-left-zero">
-						<div class="float-right">
-							<div id="self-submissions-pagination">
-							</div>
-						</div>
-					</div>
+					{{-- <div class="col-md-6 text-align-right">
+						<a data-toggle="modal" data-target="#share-like-it-list" class="btn btn-primary">Share Like It List</a>
+						<a data-toggle="modal" data-target="#invite-to-audition-modal" class="btn btn-primary"><i class="fa fa-envelope-o"></i> Invite to Audition</a>
+						<button id="remove-all-likeitlist" class="btn btn-danger"><i class="fa fa-times"></i> Remove All</button>
+					</div> --}}
 				</div>
 				<div id="loading-div">
 					<div class="f_circleG" id="frotateG_01"></div>
@@ -34,18 +27,19 @@
 					<div class="f_circleG" id="frotateG_07"></div>
 					<div class="f_circleG" id="frotateG_08"></div>
 				</div>
-				<div class="row-fluid clearfix" id="self-submissions-results">
-					@include('components.talent2', [ 'databind' => [ 'template' => '#self-submissions-results', 'value' => 'role.selfsubmissions.data' ], 'class' => 'col-lg-4 col-md-6 col-sm-6 col-xs-12', 'selfsubmission_message' => true ])
+				<div class="row-fluid clearfix" id="like-it-list-results">
+					@include('components.talent2', [ 'databind' => [ 'template' => '#like-it-list-results', 'value' => 'role.likeitlist.data' ], 'unrate' => true, 'remove' => true, 'class' => 'col-lg-3 col-md-4 col-sm-6 col-xs-12 talent-item-container' ])
 				</div>
+
 			</div> {{-- talents-search-results --}}
 		</div>
 	</div>
 </div>
 
-@include('components.modals.share-like-it-list')
 @include('components.modals.talent-message')
+@include('components.modals.talent-add-note')
+@include('components.modals.talent-edit-note')
+@include('components.modals.share-like-it-list')
 @include('components.modals.talent-photos')
 @include('components.modals.talent-resume')
 @include('components.modals.invite-to-audition')
-@include('components.modals.talent-add-note')
-@include('components.modals.talent-edit-note')
