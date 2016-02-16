7085121a73660c24187a92590e81ccd0e0080515
James Harold Canque <jamesharold.utech@gmail.com>
2016-02-16 19:22:37 +0800

    find talents page schedules modal

--- resources/views/project/role-options/role-scheduled.blade.php
+++ resources/views/project/role-options/role-scheduled.blade.php
@@ -2,12 +2,12 @@
 	<div class="row">
 		<div class="col-md-5 call-back-container">
 			<div class="panel panel-dark">
-				<div class="panel-heading">
+				<div class="panel-heading panel-heading-dark">
 					<div class="display-block margin-bottom-normal">
-						<div class="display-inline-block valign-middle"><h5 class="margin-zero"><b>Callback</b> <span class="text-muted">02/20/2016</span></h5></div>
+						<div class="display-inline-block valign-middle"><h5 class="margin-zero"><b>Callback</b> <span class="text-muted">02/20/2016</span> <span class="text-muted">, Room One</span></h5></div>
 						<div class="display-inline-block float-right">
 							<div class="btn-group btn-group-sm">
-								<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Change View <i class="fa fa-caret-down"></i></button>
+								<button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown">Change View <i class="fa fa-caret-down"></i></button>
 								<ul class="dropdown-menu">
 									<li><a href="#">Action</a></li>
 									<li><a href="#">Another action</a></li>
@@ -18,7 +18,7 @@
 					<div class="display-block">
 						<div class="display-inline-block">
 							<div class="btn-group btn-group-sm">
-								<button type="button" class="btn btn-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-plus"></i> Add <i class="fa fa-caret-down"></i></button>
+								<button type="button" class="btn btn-dark-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-plus"></i> Add <i class="fa fa-caret-down"></i></button>
 								<ul class="dropdown-menu">
 									<li><a href="#">Action</a></li>
 									<li><a href="#">Another action</a></li>
@@ -27,7 +27,7 @@
 						</div>
 						<div class="display-inline-block">
 							<div class="btn-group btn-group-sm">
-								<button type="button" class="btn btn-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-pencil"></i> Edit Schedule <i class="fa fa-caret-down"></i></button>
+								<button type="button" class="btn btn-dark-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-pencil"></i> Edit Schedule <i class="fa fa-caret-down"></i></button>
 								<ul class="dropdown-menu">
 									<li><a href="#">Action</a></li>
 									<li><a href="#">Another action</a></li>
@@ -36,7 +36,7 @@
 						</div>
 						<div class="display-inline-block">
 							<div class="btn-group btn-group-sm">
-								<button type="button" class="btn btn-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-repeat"></i> Go to... <i class="fa fa-caret-down"></i></button>
+								<button type="button" class="btn btn-dark-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-repeat"></i> Go to... <i class="fa fa-caret-down"></i></button>
 								<ul class="dropdown-menu">
 									<li><a href="#">Action</a></li>
 									<li><a href="#">Another action</a></li>
@@ -44,12 +44,38 @@
 							</div>
 						</div>
 						<div class="display-inline-block float-right">
-							<button type="button" class="btn btn-default btn-sm text-xs">Build Timeframe</button>
+							<button type="button" class="btn btn-primary btn-sm text-xs">Build Timeframe</button>
 						</div>
 					</div>
 				</div>
-				<div class="panel-body">
-					
+				<div class="panel-body padding-zero">
+					<div class="schedule-notification"></div>
+					<div class="panel-group" id="schedule-time-container">
+						<div class="panel schedule-time-item">
+							<div class="panel-heading schedule-time-header">
+								<a class="accordion-toggle display-block" data-toggle="collapse" data-parent="#schedule-time-container" href="#scheduled-time-1">
+									<div class="display-inline-block"><span class="text-bold">9:30 AM - 6:30 PM every 10 minutes</span></div>
+									<div class="display-inline-block float-right"><button class="btn btn-dark btn-xs btn-xxs text-right"><i class="fa fa-pencil"></i> Edit</button></div>
+									<div class="text-default role-name">Male Cheft Host <span class="text-bold">(2)</span></div>
+								</a>
+							</div>
+							<div id="scheduled-time-1" class="schedule-time-body panel-collapse in">
+								<div class="row-fluid clearfix time-item">
+									<div class="col-md-2 time">9:30am</div>
+									<div class="col-md-10 talent">test</div>
+								</div>
+								<div class="row-fluid clearfix time-item">
+									<div class="col-md-2 time">9:30am</div>
+									<div class="col-md-10 talent">test</div>
+								</div>
+								<div class="row-fluid clearfix time-item">
+									<div class="col-md-2 time">9:30am</div>
+									<div class="col-md-10 talent">test</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
