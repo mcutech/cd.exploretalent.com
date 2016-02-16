7d2f81cf6acce42b9a6b15d545a9b36c0bdeefa4
James Harold Canque <jamesharold.utech@gmail.com>
2016-02-16 13:56:55 +0800

    find-talents page scheduled modal ongoing

---
+++ resources/views/project/role-options/role-scheduled.blade.php
@@ -0,0 +1,59 @@
+<div class="role-find-talents-scheduled-wrapper">
+	<div class="row">
+		<div class="col-md-5 call-back-container">
+			<div class="panel panel-dark">
+				<div class="panel-heading">
+					<div class="display-block margin-bottom-normal">
+						<div class="display-inline-block valign-middle"><h5 class="margin-zero"><b>Callback</b> <span class="text-muted">02/20/2016</span></h5></div>
+						<div class="display-inline-block float-right">
+							<div class="btn-group btn-group-sm">
+								<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Change View <i class="fa fa-caret-down"></i></button>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="display-block">
+						<div class="display-inline-block">
+							<div class="btn-group btn-group-sm">
+								<button type="button" class="btn btn-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-plus"></i> Add <i class="fa fa-caret-down"></i></button>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="display-inline-block">
+							<div class="btn-group btn-group-sm">
+								<button type="button" class="btn btn-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-pencil"></i> Edit Schedule <i class="fa fa-caret-down"></i></button>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="display-inline-block">
+							<div class="btn-group btn-group-sm">
+								<button type="button" class="btn btn-outline dropdown-toggle" data-toggle="dropdown"><i class="fa fa-repeat"></i> Go to... <i class="fa fa-caret-down"></i></button>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="display-inline-block float-right">
+							<button type="button" class="btn btn-default btn-sm text-xs">Build Timeframe</button>
+						</div>
+					</div>
+				</div>
+				<div class="panel-body">
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-md-7 available-talents-container">
+		</div>
+	</div>
+</div>
