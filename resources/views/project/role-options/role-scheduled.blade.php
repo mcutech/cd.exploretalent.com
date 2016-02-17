4121123ba900d06511f7ae6c238b819224bb8b6b
James Harold Canque <jamesharold.utech@gmail.com>
2016-02-17 19:05:12 +0800

    find talents page scheduled modal v3

--- resources/views/project/role-options/role-scheduled.blade.php
+++ resources/views/project/role-options/role-scheduled.blade.php
@@ -1,10 +1,10 @@
 <div class="role-find-talents-scheduled-wrapper">
 	<div class="row">
-		<div class="col-md-5 call-back-container">
+		<div class="col-md-5 call-back-audition-container">
 			<div class="panel panel-dark">
 				<div class="panel-heading panel-heading-dark">
 					<div class="display-block margin-bottom-normal">
-						<div class="display-inline-block valign-middle"><h5 class="margin-zero"><b>Callback</b> <span class="text-muted">02/20/2016</span> <span class="text-muted">, Room One</span></h5></div>
+						<div class="display-inline-block valign-middle"><h5 class="margin-zero"><b>Audition</b> <span class="text-muted">02/20/2016</span> <span class="text-muted">, Room One</span></h5></div>
 						<div class="display-inline-block float-right">
 							<div class="btn-group btn-group-sm">
 								<button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown">Change View <i class="fa fa-caret-down"></i></button>
@@ -68,18 +68,149 @@
 									<div class="col-md-2 time">9:30am</div>
 									<div class="col-md-10 talent">test</div>
 								</div>
-								<div class="row-fluid clearfix time-item">
-									<div class="col-md-2 time">9:30am</div>
-									<div class="col-md-10 talent">test</div>
-								</div>
 							</div>
 						</div>
 					</div>
-
 				</div>
 			</div>
-		</div>
-		<div class="col-md-7 available-talents-container">
-		</div>
+		</div> {{-- call-back-audition-container --}}
+
+		<div class="col-md-7 available-talent-container padding-left-zero">
+			<div class="panel panel-dark">
+				<div class="panel-heading panel-heading-dark">
+					<div class="display-block margin-bottom-normal">
+						<div class="display-inline-block valign-middle"><h5 class="margin-zero"><b>Available Talent</b></h5></div>
+						<div class="display-inline-block float-right">
+							<div class="btn-group btn-group-sm">
+								<button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown"><i class="fa fa-gear"></i> Settings <i class="fa fa-caret-down"></i></button>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+								</ul>
+							</div>
+							<div class="btn-group btn-group-sm">
+								<button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown">Change View <i class="fa fa-caret-down"></i></button>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="display-block">
+						<div class="display-inline-block">
+							<div class="btn-group btn-group-sm">
+								<div class="form-group form-inline margin-zero">
+									<label class="label-control text-white">Role</label>
+									<select class="form-control input-xs">
+										<option>All</option>
+										<option>Female Chef Host</option>
+										<option>Male Chef Host</option>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div class="display-inline-block">
+							<div class="btn-group btn-group-sm margin-left-normal">
+								<div class="form-group form-inline margin-zero">
+									<label class="label-control text-white">Priority:</label>
+									<label class="checkbox-inline">
+										<input type="checkbox" id="" value="option1" class="px"> <span class="lbl"><span class="text-white">1</span></span>
+									</label>
+									<label class="checkbox-inline">
+										<input type="checkbox" id="" value="option2" class="px"> <span class="lbl"><span class="text-white">2</span></span>
+									</label>
+									<label class="checkbox-inline">
+										<input type="checkbox" id="" value="option3" class="px"> <span class="lbl"><span class="text-white">3</span></span>
+									</label>
+									<label class="checkbox-inline">
+										<input type="checkbox" id="" value="option4" class="px"> <span class="lbl"><span class="text-white">4</span></span>
+									</label>
+									<label class="checkbox-inline">
+										<input type="checkbox" id="" value="option5" class="px"> <span class="lbl"><span class="text-white">5</span></span>
+									</label>
+								</div>
+							</div>
+						</div>
+						<div class="display-inline-block float-right">
+							<button type="button" class="btn btn-primary btn-sm text-xs">Build Timeframe</button>
+						</div>
+					</div>
+				</div>
+				<div class="panel-body padding-zero">
+					<div class="schedule-notification"></div>
+					<div class="panel-group" id="role-list-container">
+						<div class="panel schedule-time-item">
+							<div class="panel-heading role-list-header">
+								<a class="accordion-toggle display-block" data-toggle="collapse" data-parent="#role-list-container" href="#role-list-1">
+									<div class="display-inline-block"><span class="text-bold">Female Chef Host</span></div>
+									<div class="text-default role-reference text-bold">Female / Principal / All Ethnicities / 18 - 45</div>
+									<div class="text-default role-details">Female Host for Parody Cooking show. Experience in improve comedy, as a host and cooking is a plus but not required.</div>
+								</a>
+							</div>
+							<div id="role-list-1" class="role-list-body panel-collapse in">
+							@for ($i=0; $i < 6; $i++) 
+								<div class="view-container talent-photo-view">
+
+									<div class="image-container">
+										<div class="image-holder">
+											<img src="/../images/talents-sample-image.jpg" class="img-responsive">
+										</div>
+									</div>
+									<div class="talent-stats">
+										<div class="talent-name">Brent Taylor</div>
+										<div class="talent-info">
+											<ul class="list-inline">
+												<li class="text-bold">Height:</li>
+												<li>5' 5"</li>
+												<li class="text-bold">Weight:</li>
+												<li>135 lbs.</li>
+												<li class="text-bold">Age:</li>
+												<li>20</li>
+											</ul>
+										</div>
+										<div class="talent-agency">
+											<ul class="list-inline">
+												<li class="text-bold">Agency:</li>
+												<li>Public Submission</li>
+											</ul>
+										</div>
+										<div class="role-and-confirmation">
+											<div class="talent-role">
+												<ul class="list-inline">
+													<li>Male Chef Host</li>
+												</ul>
+											</div>
+											<div class="talent-confirmation-status">
+												<div class="confirmation btn-group">
+													<button class="btn btn-default btn-success btn-xs tip" data-original-title="Confirmed" data-confirm="confirmed"><i class="fa fa-check"></i></button>
+													<button class="btn btn-default btn-xs tip" data-original-title="Declined" data-confirm="declined"><i class="fa fa-times"></i></button>
+													<button class="btn btn-default btn-xs tip" data-original-title="Rescheduled" data-confirm="reschedule"><i class="fa fa-clock-o"></i></button>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="talent-like-it-list-number">
+										<div class="like-it-list-container">
+											<div class="btn-group talent-function" data-bind-target="data-id" data-id="schedule-615535">
+												<button class="btn btn-xs btn-danger rating-button function-item" data-bind-target="class">1</button>
+												<button class="btn btn-xs btn-warning rating-button function-item" data-bind-target="class">2</button>
+												<button class="btn btn-xs btn-info rating-button function-item" data-bind-target="class">3</button>
+												<button class="btn btn-xs btn-primary rating-button function-item" data-bind-target="class">4</button>
+												<button class="btn btn-xs btn-success rating-button function-item active" data-bind-target="class">5</button>
+											</div>
+										</div>
+									</div>
+									<div class="talent-note">
+										
+									</div>
+								</div>
+							@endfor
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> {{-- available-talent-container --}}		
 	</div>
 </div>
