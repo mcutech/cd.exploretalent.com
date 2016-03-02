fdc7fb24a6058058ece13e25936cafe4c739e086
Sigmund Burgos <sigmund.burgos@gmail.com>
2016-03-02 17:28:48 +0800

    Fixed worksheet loading times. Added loading icon.

--- resources/js/event-handlers/worksheet.js
+++ resources/js/event-handlers/worksheet.js
@@ -25,7 +25,6 @@ handler.prototype.refresh = function() {
 	var data = {
 		query : [
 			[ 'with', 'bam_role.bam_casting' ],
-			[ 'with', 'bam_role.schedules' ],
 			[ 'orderBy', 'created_at', 'DESC']
 		],
 
@@ -39,21 +38,43 @@ handler.prototype.refresh = function() {
 		data.query.push([ 'where', 'status', '<=', -1 ]);
 	}
 
+	self.core.service.databind('#campaigns-list',{ data : [] });
+	$('#campaigns-list').append('<tr> <td colspan="9" class="text-center"> <h1><i class="fa fa-spinner fa-spin"></i></h1> </td> </tr>');
+
 	self.core.resource.campaign.get(data)
 		.then(function(res) {
-			console.log(res);
 			_.remove(res.data, function(campaign) {
 				return campaign.bam_role == null;
 			});
 
-			_.each(res.data, function(campaign) {
+			self.campaigns = res;
+
+			var promises = [];
+
+			_.each(self.campaigns.data, function(campaign) {
 				_.remove(campaign.bam_role.schedules, function(s) {
 					return s.rating == 0;
 				});
+
+				promises.push(self.getCampaignTalentCount(campaign));
 			});
 
-			self.core.service.databind('#campaigns-list', res);
+			return $.when.apply($, promises);
+		})
+		.then(function() {
+			self.core.service.databind('#campaigns-list', self.campaigns);
 		});
+}
+
+handler.prototype.getCampaignTalentCount = function(campaign) {
+	var deferred = $.Deferred();
+
+	campaign.bam_role.getLikeItList().then(function(res) {
+		campaign.bam_role.likeitlist = res;
+		deferred.resolve();
+	});
+
+	return deferred.promise();
 }
 
 module.exports = function(core, user) {
