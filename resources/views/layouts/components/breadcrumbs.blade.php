2c32d031175d5491a23c52dfa53f928ca738de16
James Harold Canque <jamesharold.utech@gmail.com>
2016-03-28 16:18:28 +0800

    no sidebar and top navigation layout

--- resources/views/layouts/components/breadcrumbs.blade.php
+++ resources/views/layouts/components/breadcrumbs.blade.php
@@ -1,12 +1,16 @@
-<ul class="breadcrumb breadcrumb-page">
-	<li>
-		<a href="/projects">Home</a>
-	</li>
-	@if (isset($pages))
-		@foreach ($pages as $page)
-		<li class="{{ isset($page['active']) && $page['active'] ? 'active' : '' }}">
-			<a href="{{ $page['url'] }}">{{ $page['name'] }}</a>
-		</li>
-		@endforeach
-	@endif
-</ul>
+<div class="row-fluid clearfix">
+	<div class="col-md-12">
+		<ul class="breadcrumb breadcrumb-page">
+			<li>
+				<a href="/projects">Home</a>
+			</li>
+			@if (isset($pages))
+				@foreach ($pages as $page)
+				<li class="{{ isset($page['active']) && $page['active'] ? 'active' : '' }}">
+					<a href="{{ $page['url'] }}">{{ $page['name'] }}</a>
+				</li>
+				@endforeach
+			@endif
+		</ul>		
+	</div>
+</div>
