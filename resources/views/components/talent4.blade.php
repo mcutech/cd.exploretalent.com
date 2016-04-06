de0ee772d741e355b1eddace127662e2d4a3bb98
James Harold Canque <jamesharold.utech@gmail.com>
2016-04-06 18:59:15 +0800

    photo on browse talents talents

--- resources/views/components/talent4.blade.php
+++ resources/views/components/talent4.blade.php
@@ -28,7 +28,7 @@
 		<div class="talent-note-v2">
 			<div class="back-add-note-container row-fluid clearfix">
 				<a class="back-btn btn btn-outline border-radius-zero col-xs-2 padding-left-small padding-right-small border-width-zero"><span class="text-default"><i class="fa fa-chevron-left"></i></span></a>
-				<a href="#" class="add-note-btn btn btn-outline border-radius-zero col-xs-10 padding-left-small padding-right-small border-width-zero"><span class="text-default"><i class="fa fa-plus"></i> Add Casting Note</span></a>
+				<a href="#" class="add-note-btn btn btn-outline border-radius-zero col-xs-10 padding-left-small padding-right-small border-width-zero"><span class="text-default"><i class="fa fa-plus"></i> Add Note</span></a>
 			</div>
 			<div class="note-item-container-holder">
 				<div id="schedule-notes" class="talent-item note-item-container padding-small">
@@ -65,7 +65,7 @@
 		</div>
 		<div class="talent-information-parent row-fluid clearfix" data-bind="talent-body-<%= talentnum %>" data-bind-target="id">
 			<div class="col-md-12 padding-zero">
-				<span class="name" data-bind="<%= getFullName() %>"></span>
+				<span class="name font-size-normal-zz-xs" data-bind="<%= getFullName() %>"></span>
 				<span class="age"><i>,</i><span data-bind="<%= getAge() %>" class="age-area"></span></span>
 				<div class="talent-additional-info">
 					<div class="additional-info-item height"><span>Height: </span><span data-bind="<%= getHeight() %>"></span></div>
@@ -129,7 +129,7 @@
 									</div>
 								</div>
 							</div>
-							<a href="#"><div class="add-casting-note padding-top-small padding-bottom-small bordered text-align-center"><i class="fa fa-plus"></i> Add Casting Note</div></a>
+							<a href="#"><div class="add-casting-note padding-top-small padding-bottom-small bordered text-align-center"><i class="fa fa-plus"></i> Add Note</div></a>
 						</div>
 					</div>
 					@endif
