fbddeb07f7bd7f5038fb79ecf0f2d30a85451a55
Aimee <aimee.utech@gmail.com>
2016-04-14 22:06:58 +0800

    [Change Request] Favorite Talents > Use New Talent Images

--- resources/js/controllers/talent.favorite.js
+++ resources/js/controllers/talent.favorite.js
@@ -6,4 +6,20 @@ module.exports = function(core, user){
 	$(document).on('click', '.rating-button', handler.refreshCastingRole);
 	$(document).on('change', '#casting-list', handler.selectCastingRole);
 	$(document).on('click', '#btn-add-to-likeitlist', handler.addToLikeitlist);
+
+	//talents add notes function
+	$(document).on('click', '.talent-function-icon.notes', function() {
+		$(this).closest('.talent-item').find('.talent-note-v2').show();
+		$(this).closest('.talent-item').find('.talent-photo-v2').hide();
+		$(this).closest('.talent-item').find('.talent-functions-v2 ').hide();
+	});
+
+	//add notes function back
+	$(document).on('click', '.talent-note-v2 .back-btn', function(){
+		$(this).closest('.talent-item').find('.talent-note-v2').hide();
+		$(this).closest('.talent-item').find('.talent-photo-v2').show();
+		$(this).closest('.talent-item').find('.talent-functions-v2 ').show();
+	});
+
+
 };
