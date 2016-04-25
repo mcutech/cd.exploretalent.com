ba7fc6c726a22fc01898043c879d056ecc7c37e4
Aimee <aimee.utech@gmail.com>
2016-04-25 17:17:58 +0800

    [Bug Fix] Browse Talents > Refine Search options > Hidden Search Button

--- resources/views/talent/components/filter.blade.php
+++ resources/views/talent/components/filter.blade.php
@@ -8,100 +8,95 @@
 
 		<div class="panel-body form-horizontal">
 			<div class="row text-center-zz-sm">
-				<div id="location-search-display" class="col-xs-12">
-					<div class="row">
-						<label class="control-label pull-left padding-left-normal"><a id="location-search-display-btn" href="">United States</a> <span class="padding-left-small">or</span></label>
-						<div class="col-xs-12 col-sm-4 col-md-4 margin-top-normal-zz-xs">
-							<select id="markets-list" name="markets" class="form-control" tabindex="-1" data-select multiple>
-								<option></option>
-								<option value="Albany, NY">Albany, NY</option>
-								<option value="Albuquerque, NM">Albuquerque, NM</option>
-								<option value="Atlanta, GA">Atlanta, GA</option>
-								<option value="Augusta, ME">Augusta, ME</option>
-								<option value="Baltimore, MD">Baltimore, MD</option>
-								<option value="Billings, MT">Billings, MT</option>
-								<option value="Birmingham, AL">Birmingham, AL</option>
-								<option value="Boise, ID">Boise, ID</option>
-								<option value="Boston, MA">Boston, MA</option>
-								<option value="Buffalo, NY">Buffalo, NY</option>
-								<option value="Charleston, SC">Charleston, SC</option>
-								<option value="Charleston, WV">Charleston, WV</option>
-								<option value="Charlotte, NC">Charlotte, NC</option>
-								<option value="Chicago, IL">Chicago, IL</option>
-								<option value="Cleveland, OH">Cleveland, OH</option>
-								<option value="Columbia, SC">Columbia, SC</option>
-								<option value="Columbus, OH">Columbus, OH</option>
-								<option value="Dallas, TX">Dallas, TX</option>
-								<option value="Denver, CO">Denver, CO</option>
-								<option value="Des Moines, IA">Des Moines, IA</option>
-								<option value="Detroit, MI">Detroit, MI</option>
-								<option value="El Paso, TX">El Paso, TX</option>
-								<option value="Fargo, ND">Fargo, ND</option>
-								<option value="Grand Junct, CO">Grand Junct, CO</option>
-								<option value="Harford, CT">Harford, CT</option>
-								<option value="Honolulu, HI">Honolulu, HI</option>
-								<option value="Houston, TX">Houston, TX</option>
-								<option value="Indianapolis, IN">Indianapolis, IN</option>
-								<option value="Jackson, MS">Jackson, MS</option>
-								<option value="Jacksonville, FL">Jacksonville, FL</option>
-								<option value="Kansas City, KS">Kansas City, KS</option>
-								<option value="Kansas City, MO">Kansas City, MO</option>
-								<option value="Las Vegas, NV">Las Vegas, NV</option>
-								<option value="Little Rock, AR">Little Rock, AR</option>
-								<option value="Los Angeles, CA">Los Angeles, CA</option>
-								<option value="Louisville, KY">Louisville, KY</option>
-								<option value="Memphis, TN">Memphis, TN</option>
-								<option value="Miami, FL">Miami, FL</option>
-								<option value="Milwaukee, WI">Milwaukee, WI</option>
-								<option value="Minneapolis, MN">Minneapolis, MN</option>
-								<option value="Nashville, TN">Nashville, TN</option>
-								<option value="New Orleans, LA">New Orleans, LA</option>
-								<option value="New York City, NY">New York City, NY</option>
-								<option value="Norfolk, VA">Norfolk, VA</option>
-								<option value="Oklahoma City, OK">Oklahoma City, OK</option>
-								<option value="Omaha, NE">Omaha, NE</option>
-								<option value="Orlando, FL">Orlando, FL</option>
-								<option value="Philadelphia, PA">Philadelphia, PA</option>
-								<option value="Phoenix, AZ">Phoenix, AZ</option>
-								<option value="Pittsburgh, PA">Pittsburgh, PA</option>
-								<option value="Portland, ME">Portland, ME</option>
-								<option value="Portland, OR">Portland, OR</option>
-								<option value="Raleigh, NC">Raleigh, NC</option>
-								<option value="Rapid City, SD">Rapid City, SD</option>
-								<option value="Reno, NV">Reno, NV</option>
-								<option value="St Louis, MO">St Louis, MO</option>
-								<option value="Salt Lake City, UT">Salt Lake City, UT</option>
-								<option value="San Antonio, TX">San Antonio, TX</option>
-								<option value="San Diego, CA">San Diego, CA</option>
-								<option value="San Francisco, CA">San Francisco, CA</option>
-								<option value="Seattle, WA">Seattle, WA</option>
-								<option value="Tampa, FL">Tampa, FL</option>
-								<option value="Washington, DC">Washington, DC</option>
-								<option value="Wichita, KS">Wichita, KS</option>
-								<option value="All Markets">All Markets</option>
-								<option value="Calgary-Edm, AB">Calgary-Edm, AB</option>
-								<option value="Ottawa, ON">Ottawa, ON</option>
-								<option value="Montreal, QC">Montreal, QC</option>
-								<option value="Toronto, ON">Toronto, ON</option>
-								<option value="Vancouver, BC">Vancouver, BC</option>
-							</select>
-						</div>
-						<div class="col-xs-12 col-sm-3 col-md-3 margin-top-normal-zz-xs">
-							<select name="distance" class="form-control">
-								<option value="25">25 miles</option>
-								<option value="50">50 miles</option>
-								<option value="100">100 miles</option>
-								<option value="150">150 miles</option>
-								<option value="200">200 miles</option>
-							</select>
-						</div>
-						<div class="col-xs-12 col-sm-2 margin-top-normal-zz-xs">
-							<button id="search-button" type="submit" class="btn btn-primary btn-block">Search</button>
-						</div>
+				<div id="location-search-display" class="col-md-9">
+					<label class="control-label pull-left padding-left-normal"><a id="location-search-display-btn" href="">United States</a> <span class="padding-left-small">or</span></label>
+					<div class="col-md-3 margin-top-normal-zz-xs">
+						<select id="markets-list" name="markets" class="form-control" tabindex="-1" data-select multiple data-bind="<%= bam_casting.market.split('>').join('|') %>">
+							<option> </option>
+							<option value="Albany, NY">Albany, NY</option>
+							<option value="Albuquerque, NM">Albuquerque, NM</option>
+							<option value="Atlanta, GA">Atlanta, GA</option>
+							<option value="Augusta, ME">Augusta, ME</option>
+							<option value="Baltimore, MD">Baltimore, MD</option>
+							<option value="Billings, MT">Billings, MT</option>
+							<option value="Birmingham, AL">Birmingham, AL</option>
+							<option value="Boise, ID">Boise, ID</option>
+							<option value="Boston, MA">Boston, MA</option>
+							<option value="Buffalo, NY">Buffalo, NY</option>
+							<option value="Charleston, SC">Charleston, SC</option>
+							<option value="Charleston, WV">Charleston, WV</option>
+							<option value="Charlotte, NC">Charlotte, NC</option>
+							<option value="Chicago, IL">Chicago, IL</option>
+							<option value="Cleveland, OH">Cleveland, OH</option>
+							<option value="Columbia, SC">Columbia, SC</option>
+							<option value="Columbus, OH">Columbus, OH</option>
+							<option value="Dallas, TX">Dallas, TX</option>
+							<option value="Denver, CO">Denver, CO</option>
+							<option value="Des Moines, IA">Des Moines, IA</option>
+							<option value="Detroit, MI">Detroit, MI</option>
+							<option value="El Paso, TX">El Paso, TX</option>
+							<option value="Fargo, ND">Fargo, ND</option>
+							<option value="Grand Junct, CO">Grand Junct, CO</option>
+							<option value="Harford, CT">Harford, CT</option>
+							<option value="Honolulu, HI">Honolulu, HI</option>
+							<option value="Houston, TX">Houston, TX</option>
+							<option value="Indianapolis, IN">Indianapolis, IN</option>
+							<option value="Jackson, MS">Jackson, MS</option>
+							<option value="Jacksonville, FL">Jacksonville, FL</option>
+							<option value="Kansas City, KS">Kansas City, KS</option>
+							<option value="Kansas City, MO">Kansas City, MO</option>
+							<option value="Las Vegas, NV">Las Vegas, NV</option>
+							<option value="Little Rock, AR">Little Rock, AR</option>
+							<option value="Los Angeles, CA">Los Angeles, CA</option>
+							<option value="Louisville, KY">Louisville, KY</option>
+							<option value="Memphis, TN">Memphis, TN</option>
+							<option value="Miami, FL">Miami, FL</option>
+							<option value="Milwaukee, WI">Milwaukee, WI</option>
+							<option value="Minneapolis, MN">Minneapolis, MN</option>
+							<option value="Nashville, TN">Nashville, TN</option>
+							<option value="New Orleans, LA">New Orleans, LA</option>
+							<option value="New York City, NY">New York City, NY</option>
+							<option value="Norfolk, VA">Norfolk, VA</option>
+							<option value="Oklahoma City, OK">Oklahoma City, OK</option>
+							<option value="Omaha, NE">Omaha, NE</option>
+							<option value="Orlando, FL">Orlando, FL</option>
+							<option value="Philadelphia, PA">Philadelphia, PA</option>
+							<option value="Phoenix, AZ">Phoenix, AZ</option>
+							<option value="Pittsburgh, PA">Pittsburgh, PA</option>
+							<option value="Portland, ME">Portland, ME</option>
+							<option value="Portland, OR">Portland, OR</option>
+							<option value="Raleigh, NC">Raleigh, NC</option>
+							<option value="Rapid City, SD">Rapid City, SD</option>
+							<option value="Reno, NV">Reno, NV</option>
+							<option value="St Louis, MO">St Louis, MO</option>
+							<option value="Salt Lake City, UT">Salt Lake City, UT</option>
+							<option value="San Antonio, TX">San Antonio, TX</option>
+							<option value="San Diego, CA">San Diego, CA</option>
+							<option value="San Francisco, CA">San Francisco, CA</option>
+							<option value="Seattle, WA">Seattle, WA</option>
+							<option value="Tampa, FL">Tampa, FL</option>
+							<option value="Washington, DC">Washington, DC</option>
+							<option value="Wichita, KS">Wichita, KS</option>
+							<option value="All Markets">All Markets</option>
+							<option value="Calgary-Edm, AB">Calgary-Edm, AB</option>
+							<option value="Ottawa, ON">Ottawa, ON</option>
+							<option value="Montreal, QC">Montreal, QC</option>
+							<option value="Toronto, ON">Toronto, ON</option>
+							<option value="Vancouver, BC">Vancouver, BC</option>
+						</select>
+					</div>
+					<div class="col-md-2 margin-top-normal-zz-xs">
+						<select name="distance" class="form-control">
+							<option value="25">25 miles</option>
+							<option value="50">50 miles</option>
+							<option value="100">100 miles</option>
+							<option value="150">150 miles</option>
+							<option value="200">200 miles</option>
+						</select>
 					</div>
 				</div>
 
-				<div id="location-search-change" class="col-xs-12 col-md-9" hidden>
+				<div id="location-search-change" class="col-md-9" hidden >
 					<div class="col-md-12 search-result-counter">
 						<div class="hide bordered no-border-vr border-left-width-zero display-inline-block padding-right-normal margin-right-normal-sm-lg border-zero-zz-sm">
 							<h2 class="margin-zero text-bold">5,000</h2>
@@ -114,13 +109,9 @@
 						</div>
 					</div>
 				</div>
-			</div>
 
-			<div class="row">
-				<div class="margin-top-small margin-bottom-small" id="selected-markets">
-					<label class="checkbox-inline margin-right-small">
-						<input type="checkbox" name="market-checks" class="px check-markets"> <span class="lbl">Albany, NY</span>
-					</label>
+				<div class="col-md-2 pull-right margin-top-normal-zz-xs">
+					<button id="search-button" type="submit" class="btn btn-primary btn-block">Search</button>
 				</div>
 			</div>
 
