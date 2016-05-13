378bdb752024eb517a4bdbdaffdb3076ac91c315
Barry Deeney <mxaddict@codedmaster.com>
2016-05-14 02:28:15 +0800

    Fixed oauth logic

--- resources/js/controllers/login.js
+++ resources/js/controllers/login.js
@@ -37,7 +37,7 @@ module.exports = function(core, user) {
 
 					localStorage.setItem('access_token', result.access_token);
 					localStorage.setItem('refresh_token', result.refresh_token);
-					localStorage.setItem('access_date', new Date());
+					localStorage.setItem('expires_on', Math.round(new Date().getTime() / 1000) + result.expires_in);
 					window.location = qs.redirect ? decodeURIComponent(qs.redirect) : '/projects';
 				},
 				function(error){
