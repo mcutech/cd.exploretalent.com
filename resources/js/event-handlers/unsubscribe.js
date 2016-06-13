3e5351c27113d880de1e382be6dfd66d85debc34
Daniel Cabale <dc.utech@gmail.com>
2016-06-13 21:14:20 +0800

    issue-387-v1

---
+++ resources/js/event-handlers/unsubscribe.js
@@ -0,0 +1,49 @@
+'use strict';
+var _ = require('lodash');
+
+function handler(core, user){
+	self = this;
+	self.core = core;
+	self.user = user;
+}
+
+handler.prototype.saveChanges = function() {
+	console.log(self.user);
+
+	var data = {
+		query : [
+			['where', 'bam_cd_user_id', self.user.bam_cd_user_id]
+		]
+	}
+
+	self.core.resource.cd_user_subscription.get(data)
+	.then(function(res) {
+		console.log(res)
+		if(res.total==0){
+			var data = {
+				bam_cd_user_id : self.user.user_id,
+				sms : $('#emailchck').is(':checked') ? 0 : 1,
+				email : $('#smschck').is(':checked') ? 0 : 1
+			}
+			self.core.resource.cd_user_subscription.post(data)
+			.then(function(res) {
+				window.location = '/';
+			});
+		} else {
+			var data = {
+				subscriptionId: res.data[0].id,
+				sms : $('#emailchck').is(':checked') ? 0 : 1,
+				email : $('#smschck').is(':checked') ? 0 : 1
+			}
+			self.core.resource.cd_user_subscription.patch(data)
+			.then(function(res) {
+				console.log(res);
+				window.location = '/';
+			});
+		}
+	});
+}
+
+module.exports = function(core, user) {
+	return new handler(core, user);
+};
---
+++ resources/views/layouts/unsubscribe-navbar.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.master')
+
+@section('master.body')
+
+<div id="main-wrapper">
+	<div id="main-navbar"
+		class="navbar navbar-inverse"
+		role="navigation">
+
+		<div class="navbar-inner">
+
+			<div class="navigation-header-container">
+				<div class="navbar-header">
+					<a href="/projects"
+						class="navbar-brand">
+						<img src="/images/logo-home-et-cd.png" width="180px" class="logo-for-desktop">
+						<!-- <img src="../images/logo-home-et-cd-mobile2.png" class="logo-for-mobile"> -->
+					</a>
+				</div>
+				<div class="navbar-navigations">
+					<ul class="nav navbar-nav primary-nav">
+					</ul>
+				</div>
+
+				<button type="button"
+					class="navbar-toggle collapsed"
+					data-toggle="collapse"
+					data-target="#main-navbar-collapse">
+					<i class="navbar-icon fa fa-bars"></i>
+				</button>
+
+				<div id="main-navbar-collapse" class="collapse navbar-collapse main-navbar-collapse">
+					<ul class="nav navbar-nav pull-right right-navbar-nav secondary-nav">
+						<li>
+							<a href="#" class="nav-link logout"><i class="dropdown-icon fa fa-power-off"></i> <span class="secondary-nav-text">Logout</span></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	@yield('navbar.body')
+</div>
+@stop
+
