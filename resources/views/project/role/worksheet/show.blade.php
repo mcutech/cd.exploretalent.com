31d604695a302bf22b1bc6ec42bfef7820fb199f
James Harold Canque <jamesharold.utech@gmail.com>
2016-07-28 22:21:01 +0800

    fix worksheet image responsive

--- resources/views/project/role/worksheet/show.blade.php
+++ resources/views/project/role/worksheet/show.blade.php
@@ -27,13 +27,13 @@
 
 			<div class="col-md-9 audition-worksheet-talent-item">
 			<div id="worksheet-pagination" class="text-right"></div>
-				<div class="margin-bottom-small header-talent-item">
+				<div class="margin-bottom-small header-talent-item display-none-zz-xs">
 					<div class="padding-normal padding-top-small padding-bottom-small">
 						<div class="row-fluid clearfix">
-							<div class="col-md-2">Talent</div>
-							<div class="col-md-5">Role / Schedules</div>
-							<div class="col-md-3">Notes</div>
-							<div class="col-md-2">Actions</div>
+							<div class="col-sm-2">Talent</div>
+							<div class="col-sm-5">Role / Schedules</div>
+							<div class="col-sm-3">Notes</div>
+							<div class="col-sm-2">Actions</div>
 						</div>
 					</div>
 				</div>
@@ -41,8 +41,10 @@
 					<div class="panel schedule margin-bottom-small" data-bind-template="#schedules" data-bind-value="data" data-bind="<%= id %>" data-bind-target="data-id">
 						<div class="panel-body">
 							<div class="row-fluid clearfix">
-								<div class="col-md-2 talent-content margin-bottom">
-									<div class="talent-name" data-bind="<%= invitee.bam_talentci.getFullName() %>">
+								<div class="col-xs-3 display-none-sm-lg"></div>
+								<div class="col-xs-6 col-sm-2 talent-content margin-bottom">
+									<div class="talent-name text-align-center">
+										<b><span data-bind="<%= invitee.bam_talentci.getFullName() %>"></span></b>
 									</div>
 									<div class="talent-photo photo-user-container">
 										<img data-bind="<%= invitee.bam_talentci.getPrimaryPhoto() %>" alt="" class="img-responsive">
@@ -61,8 +63,9 @@
 										</div>
 									</div>
 								</div> <!--/talent-content-->
+								<div class="col-xs-3 display-none-sm-lg"></div>
 
-								<div class="col-md-5 role-schedule-content">
+								<div class="col-xs-12 col-sm-5 padding-left-zero-zz-xs padding-right-zero-zz-xs role-schedule-content margin-top-normal-zz-xs">
 									<div class="role-name margin-bottom-small">
 										<span class="text-semibold">Role: </span><a data-bind="/projects/<%= bam_role.casting_id %>/roles/<%= bam_role.role_id %>/edit" ><span data-bind="<%= bam_role.name %>"></span></a>
 									</div>
@@ -94,9 +97,9 @@
 										<span class="text-semibold">Schedule: </span><span data-bind="<%= when %>"></span>
 									</div>
 									<div class="btn-functions">
-										<div class="btn-group">
-											<button type="button" class="btn btn-sm accept-button" data-bind="<%= invitee_status == 2 ? 'btn-success' : 'btn-outline' %>" data-bind-target="class"><i class="fa fa-check"></i> Confirmed</button>
-											<button type="button" class="btn btn-sm decline-button" data-bind="<%= invitee_status == 3 ? 'btn-danger' : 'btn-outline' %>" data-bind-target="class"><i class="fa fa-times"></i> Declined</button>
+										<div class="row-fluid clearfix">
+											<button type="button" class="col-xs-6 col-sm-12 btn-block btn btn-sm accept-button padding-top-normal-zz-xs padding-bottom-normal-zz-xs font-size-normal-medium-zz-xs" data-bind="<%= invitee_status == 2 ? 'btn-success' : 'btn-outline' %>" data-bind-target="class"><i class="fa fa-check"></i> Confirmed</button>
+											<button type="button" class="col-xs-6 col-sm-12 btn-block btn btn-sm decline-button  padding-top-normal-zz-xs padding-bottom-normal-zz-xs font-size-normal-medium-zz-xs" data-bind="<%= invitee_status == 3 ? 'btn-danger' : 'btn-outline' %>" data-bind-target="class"><i class="fa fa-times"></i> Declined</button>
 											<!--
 											<button type="button" class="btn btn-sm reschedule-button" data-bind="<%= invitee_status == 4 ? 'btn-success' : 'btn-outline' %>" data-bind-target="class" data-toggle="modal" data-target="#reschedule-modal"><i class="fa fa-clock-o"></i> Reschedule</button>
 											-->
@@ -104,20 +107,20 @@
 									</div>
 								</div><!--/role-schedule-content-->
 
-								<div class="col-md-3 notes-content">
+								<div class="col-xs-12 col-sm-3 padding-zero-zz-xs notes-content margin-top-normal-zz-sm margin-bottom-normal-zz-sm">
 									<div id="notes">
 										<div class="notes-container margin-bottom-small" data-bind-template="#notes" data-bind-value="schedule_notes" data-bind="<%= body %>">
 										</div>
 									</div>
 									<div class="notes-action">
-										<button data-target="#add-note-modal" data-toggle="modal" class="btn btn-outline btn-sm add-note-button"><i class="fa fa-pencil"></i>  Add Note</button>
+										<button data-target="#add-note-modal" data-toggle="modal" class="btn btn-outline btn-sm add-note-button btn-block padding-top-normal-zz-xs padding-bottom-normal-zz-xs font-size-normal-medium-zz-xs"><i class="fa fa-pencil"></i>  Add Note</button>
 									</div>
 								</div>
 
-								<div class="col-md-2 actions-content">
-									<button type="button" class="btn btn-sm btn-block margin-bottom-small callback-button" data-bind="<%= status == 2 ? 'btn-success' : 'btn-outline' %>" data-bind-target="class"><i class="fa fa-clock-o"></i> Callback</button>
-									<button type="button" class="btn btn-sm btn-block margin-bottom-small hired-button" data-bind="<%= status == 3 ? 'btn-success' : 'btn-outline' %>" data-bind-target="class"><i class="fa fa-thumbs-o-up"></i> Hired</button>
-									<button type="button" class="btn btn-sm btn-outline btn-block message-button" data-toggle="modal" data-target="#message-modal"><i class="fa fa-envelope-o"></i> Message</button>
+								<div class="col-sm-2 padding-zero-zz-xs actions-content">
+									<button type="button" class="padding-top-normal-zz-xs padding-bottom-normal-zz-xs btn btn-sm btn-block margin-bottom-small callback-button" data-bind="<%= status == 2 ? 'btn-success' : 'btn-outline' %>" data-bind-target="class"><i class="fa fa-clock-o"></i> Callback</button>
+									<button type="button" class="padding-top-normal-zz-xs padding-bottom-normal-zz-xs btn btn-sm btn-block margin-bottom-small hired-button" data-bind="<%= status == 3 ? 'btn-success' : 'btn-outline' %>" data-bind-target="class"><i class="fa fa-thumbs-o-up"></i> Hired</button>
+									<button type="button" class="padding-top-normal-zz-xs padding-bottom-normal-zz-xs btn btn-sm btn-outline btn-block message-button" data-toggle="modal" data-target="#message-modal"><i class="fa fa-envelope-o"></i> Message</button>
 								</div><!--/actions-content-->
 							</div>
 						</div>
