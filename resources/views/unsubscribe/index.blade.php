a879004a02ef1a08f46af20aaecb779d80c46ad5
Luke Magsoling <lukemagsoling@gmail.com>
2016-09-29 01:14:57 +0800

    [Project] CD Notifications for emails > Unsubscribe page functionality

--- resources/views/unsubscribe/index.blade.php
+++ resources/views/unsubscribe/index.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.unsubscribe-header')
 
 @section('sidebar.body')
-	<div class="panel panel-primary" id="talent-email">
+	<div class="panel panel-primary">
 		<div class="panel-heading">
 			<label for="">Unsubscribe</label>
 		</div>
 		<div class="panel-body">
 			<div class="col-md-12">
-				<div class="">
+				<div id="talent-email">
 					<label for=""><i class="fa fa-envelope-o " aria-hidden="true"></i> Unsubscribe all Emails:</label> <span data-bind="<%= email %>"></span>
 				</div>
-				<div class="col-md-12 checkbox">
+				<div class="col-md-12 checkbox" id="unsubscribe-emails">
 					<div>
 						<label>
-							<input type="checkbox" id="emailchck" value="1" name="chckbox"> I would like to stop receiving emails from exploretalent.com
+							<input type="checkbox" id="emailchck"  name="chckbox" data-bind="<%= email %>"> I would like to stop receiving emails from exploretalent.com
 						</label>
 					</div>
 					<div>
 						<label>
-							<input type="checkbox" id="smschck" value="1" name="chckbox"> I would like to stop receiving SMS from exploretalent.com
+							<input type="checkbox" id="smschck" name="chckbox" data-bind="<%= sms %>"> I would like to stop receiving SMS from exploretalent.com
 						</label>
 					</div>
 				</div>
@@ -26,15 +26,15 @@
 				<div>
 					<label for=""></i> Select which Emails to turn off:</label>
 				</div>
-				<div class="col-md-12 checkbox">
+				<div class="col-md-12 checkbox" id="notifications">
 					<div>
 						<label>
-							<input type="checkbox" id="" value="1" name=""> Send me an Email when a talent replies.
+							<input type="checkbox" id="talentReply" value="1" data-bind="<%= data[0].value %>" > Send me an Email when a talent replies.
 						</label>
 					</div>
 					<div>
 						<label>
-							<input type="checkbox" id="" value="1" name=""> Send me an Email when a talent submits to a casting.
+							<input type="checkbox" id="talentSubmit" value="1" data-bind="<%= data[1].value%>"> Send me an Email when a talent submits to a casting.
 						</label>
 					</div>
 				</div>
