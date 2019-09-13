525c174eb10b5250a4d9e06b3c69ce9747c9114e
Alicia Escarda <escardaaliciamae@gmail.com>
2019-09-14 02:07:16 +0800

    [Feature-Update] Make new page called  and integrate with the user_address API

--- resources/js/event-handlers/alternate-emails.js
+++ resources/js/event-handlers/alternate-emails.js
@@ -2,6 +2,123 @@
 'use strict'
 
 function handler (core, user) {
+  user
+  self = this
+  self.core = core
+  self.getAllEmails()
+  self.user = user
+  console.log(user)
+}
+
+handler.prototype.getAllEmails = function () {
+  let qs = self.core.service.query_string()
+  let emails = {
+    query: [
+      ['user_id', user.id],
+      ['orderBy', 'created_at', 'DESC']
+    ],
+    page: qs.page || 1,
+    per_page: 10
+  }
+
+  self.core.resource.user_email.get(emails)
+    .then(function (res) {
+      console.log(res)
+      self.core.service.databind('#alt_emails', res)
+      self.core.service.paginate('#pagination', { total: res.total, class: 'pagination', name: 'page', per_page: res.per_page })
+      $('.email').val('')
+    })
+}
+
+handler.prototype.addEmail = function () {
+  let email = $('.email').val()
+
+  let data = {
+    cdUserId: user.bam_cd_user.user_id,
+    email: email
+  }
+  if (email.length > 0) {
+    self.core.resource.user_email.post(data)
+      .then(function (res) {
+        $.growl.notice({
+          title: '<i class=\'fa fa-check\'> </i> Updated succesfully!',
+          message: ''
+        })
+        self.getAllEmails()
+        $('.email').text('')
+      }, function (error) {
+        $.growl.error({
+          title: '<i class=\'fa fa-times\'> </i> Invalid email address!',
+          message: ''
+        })
+      })
+  }
+}
+
+handler.prototype.setEmail = function (id) {
+  self.core.resource.user_email.get({
+    user_id: user.id,
+    id: id
+  })
+    .then(function (res) {
+      let data = {
+        cdUserId: user.bam_cd_user.user_id,
+        email1: res.email
+      }
+      self.core.resource.cd_user.patch(data)
+        .then(function (res1) {
+          console.log(res1)
+        })
+
+      self.core.resource.user_email.delete({ id: id })
+        .then(function (res2) {
+          self.getAllEmails()
+          $.growl.notice({
+            title: '<i class=\'fa fa-check\'> </i> Updated succesfully!',
+            message: ''
+          })
+        })
+    })
+}
+
+handler.prototype.setEmailSec = function (id) {
+  self.core.resource.user_email.get({
+    user_id: user.id,
+    id: id
+  })
+    .then(function (res) {
+      let data = {
+        cdUserId: user.bam_cd_user.user_id,
+        email2: res.email
+      }
+      self.core.resource.cd_user.patch(data)
+        .then(function (res1) {
+          console.log(res1)
+        })
+
+      self.core.resource.user_email.delete({ id: id })
+        .then(function (res2) {
+          self.getAllEmails()
+          $.growl.notice({
+            title: '<i class=\'fa fa-check\'> </i> Updated succesfully!',
+            message: ''
+          })
+        })
+    })
+}
+
+handler.prototype.deleteEmail = function (id) {
+  if (confirm('Are you sure you want to delete this email?')) {
+    self.core.resource.user_email.delete({ id: id })
+      .then(function (res) {
+        console.log(res)
+        self.getAllEmails()
+        $.growl.notice({
+          title: '<i class=\'fa fa-check\'> </i> Deleted successfully!',
+          message: ''
+        })
+      })
+  }
 }
 
 module.exports = function (core, user) {
--- resources/views/profile/alternate-emails.blade.php
+++ resources/views/profile/alternate-emails.blade.php
@@ -5,28 +5,36 @@
 @stop
 
 @section('sidebar.body')
-<div id="settings" class="row">
-    <div class="col-md-2">
-        <input class="form-control">
+<div id="settings" class="panel-body">
+    <div class="input-group col-md-3 margin-bottom-small">
+            <input class="form-control email">
+        <div class="input-group-btn">
+            <button class="btn btn-success" id="add_email">Add</button>
+        </div>
     </div>
-    <div class="btn-success row btn margin-bottom-normal">Add Email</div>
-    <div class="row col-md-12">
-        <table class="table table-bordered">
+
+    <div class="row col-md-8">
+        <table class="table table-bordered table-striped">
         <thead>
         <tr>
             <th>Emails</th>
             <th>Action</th>
         </tr>
         </thead>
-        <tr>
-        <td>alicia.utech@gmail.com</td>
-        <td>
-            <div class="btn btn-default">Set as primary</div>
-            <div class="btn btn-default">Set as secondary</div>
-            <div class="btn btn-default">Delete</div>
-        </td>
-        </tr>
+            <tbody id="alt_emails">
+                <tr data-bind-template="#alt_emails" data-bind-value="data">
+                    <td data-bind="<%= email %>"></td>
+                    <td data-bind="<%= id %>" data-bind-target="id">
+                        <div class="btn btn-primary set-email" value="1">Set as primary</div>
+                        <div class="btn btn-warning set-email-sec" value="0">Set as secondary</div>
+                        <div class="btn btn-danger delete">Delete</div>
+                    </td>
+                </tr>
+            </tbody>
         </table>
+        <div class="col-xs-12 col-sm-12">
+            <div id="pagination" class="pull-right margin-top-large"></div>
+        </div>
     </div>
 
 </div>
